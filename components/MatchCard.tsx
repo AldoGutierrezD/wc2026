8addf361ec744f2bab9a55fbdddb802920e487be
AldoGutierrezD <aldogd24@gmail.com>
2026-06-24 15:41:41 -0600

    Se activa temporalmente el botón de guardar resultado

--- components/MatchCard.tsx
+++ components/MatchCard.tsx
@@ -163,7 +163,12 @@ export default function MatchCard({ matchId, predictions, group, stage, date, ho
                                 }))}
                             />
                             {saved[player] ? (
-                                <CheckCheck size={20} className="text-[#00c752] font-bold" />
+                                <button
+                                    onClick={() => handleSave(player)}
+                                    className="text-sm bg-[#00c752] text-white px-2 py-1 rounded cursor-pointer hover:shadow-md"
+                                >
+                                    <CircleCheckBig size={16} />
+                                </button>
                             ) : (
                                 <button
                                     onClick={() => handleSave(player)}
